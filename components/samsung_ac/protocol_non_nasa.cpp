86a1749100533c888acf961c4d1f4e561bf52425
Flytof <114093772+Flytof@users.noreply.github.com>
2025-12-04 16:05:35 +0100

    Update protocol_non_nasa.cpp

--- components/samsung_ac/protocol_non_nasa.cpp
+++ components/samsung_ac/protocol_non_nasa.cpp
@@ -405,7 +405,7 @@ namespace esphome
             data[7] = (uint8_t)encode_request_mode(mode);
             data[8] = !power ? (uint8_t)0xC0 : (uint8_t)0xF0;
             data[8] |= (individual ? 6U : 4U);
-            data[9] = (uint8_t)0x21;
+            data[9] = (uint8_t)0x01;
             data[12] = build_checksum(data);
 
             data[9] = (uint8_t)0x21;
@@ -768,6 +768,7 @@ namespace esphome
         }
     } // namespace samsung_ac
 } // namespace esphome
+
 
 
 
